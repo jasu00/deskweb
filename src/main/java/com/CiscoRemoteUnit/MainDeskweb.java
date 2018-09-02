3dce747f76e00609e9369e6f7acff765f22e0ccc
hp pc <hp pc@hp>
2018-09-02 15:17:06 +0530

    SpringBoot Initial Commit

---
+++ src/main/java/com/CiscoRemoteUnit/MainDeskweb.java
@@ -0,0 +1,14 @@
+package com.CiscoRemoteUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainDeskweb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainDeskweb.class, args);
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home Page</title>
+</head>
+<body>
+	<h1>Deskweb Home Page</h1>
+	<hr>
+	<h3>Your name is ${name}</h3>
+</body>
+</html>
